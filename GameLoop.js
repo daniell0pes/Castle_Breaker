a4c582be59dccea7b40979623b41eaebb6e5b6cb
daniell0pes <105875749+daniell0pes@users.noreply.github.com>
2022-05-27 15:17:55 +0100

    Novo Player + ajuste EventListeners + Ajuste de Frames + Melhoria de Código

--- GameLoop.js
+++ GameLoop.js
@@ -1,6 +1,6 @@
 const map = new Level(0,0,canvas.width,canvas.height);
 const foreground = new Level(0,0,canvas.width,canvas.height);
-const player = new Player(220,450,200,200);
+const player = new Player(220,450,150,150);
 
 player.load(30,6,"Assets/Homer/idle/idle_down.png", "Assets/Homer/idle/idle_left.png",
     "Assets/Homer/idle/idle_right.png", "Assets/Homer/idle/idle_up.png", 'Assets/Homer/walk/walk_down.png', 'Assets/Homer/walk/walk_left.png', 'Assets/Homer/walk/walk_right.png',
@@ -10,7 +10,7 @@ player.load(30,6,"Assets/Homer/idle/idle_down.png", "Assets/Homer/idle/idle_left
 map.load("Assets/room1.png","Assets/room2.png","Assets/room3.png","Assets/room4.png","Assets/room5.png","Assets/room6.png");
 foreground.load("Assets/room1foreground.png","Assets/room2foreground.png","Assets/room3foreground.png","Assets/room4foreground.png","Assets/room5foreground.png","Assets/room6foreground.png")
 
-const fps=30;
+const fps=60;
 function animate(){
     setTimeout(function () {
     requestAnimationFrame(animate);
